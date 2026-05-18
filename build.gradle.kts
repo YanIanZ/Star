8aef52451dd93ddb739583609f30695da54d098c
Yan <Yan>
2026-05-18 14:30:57 +0700

    chore: bump version to 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 group = "dev.yanianz"
-version = "1.0.0"
+    version = "1.0.1"
 
 allprojects {
     group = "dev.yanianz"
-    version = "1.0.0"
+version = "1.0.1"
 
     repositories {
         mavenCentral()
